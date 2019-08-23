c65c34e23ae6d326c4efe3c30a085b1c54a2c4e5
Brian Yu <brianyu28@gmail.com>
2019-08-23 17:51:25 -0400

    Add manifest, update version to 0.1.5

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
 setup(
     author="Brian Yu",
     author_email="brian@brianyu.me",
@@ -7,6 +13,7 @@ setup(
     entry_points={
         "console_scripts": ["vitality=vitality.__main__:main"]
     },
+    include_package_data=True,
     install_requires=[
         "argparse>=1.4",
         "jinja2>=2.10",
@@ -15,9 +22,11 @@ setup(
         "termcolor>=1.1",
         "watchdog>=0.9"
     ],
+    license=license,
+    long_description=readme,
     name="vitality",
     packages=["vitality"],
     url="https://github.com/brianyu28/vitality",
-    version="0.1.4"
+    version="0.1.5"
 )
 
